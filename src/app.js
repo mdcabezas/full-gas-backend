192b824f76b042cf8d01e6ca03b7b1b1e2492634
mdcabezas <manuelcabezas@gmail.com>
2023-08-14 03:03:45 +0000

    fix: change no route

--- src/app.js
+++ src/app.js
@@ -34,6 +34,9 @@ app.use("/posts", postsRoutes);
 app.use("/products", productsRoutes);
 app.use("/purchases", purchasesRoutes);
 
-app.use('*', (_, res) => res.status(404).json({ code: 404, message: "Esta ruta no existe" }));
+// app.use('*', (_, res) => res.status(404).json({ code: 404, message: "Esta ruta no existe" }));
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404</h1><h3>Route Not Found</h3>");
+  });
 
 module.exports = app;
