f952675fe8c0e01aa6ca600a395aabed2729ccf6
mdcabezas <manuelcabezas@gmail.com>
2023-08-14 02:17:41 +0000

    fix: new route users

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(cors());
 app.use(express.json())
 
 // Add routes
-app.use("/", usersRoutes);
+app.use("/users", usersRoutes);
 app.use("/posts", postsRoutes);
 app.use("/products", productsRoutes);
 app.use("/purchases", purchasesRoutes);
