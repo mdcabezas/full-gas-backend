6590b621e030fe0144f1a7d2369a321dd5f6489d
mdcabezas <manuelcabezas@gmail.com>
2023-08-14 20:17:02 +0000

    feat: add user id login

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -12,7 +12,7 @@ const signIn = async (req, res) => {
     const {code, user, message} = await verifyCredentials(email, password)
     if (code !== 200) return res.status(code).send({message: message})
     const token = jwt.sign({ email, usuario:user.id }, process.env.SECRET_JWT)
-    return res.json({token, nombre:user.nombre, email:user.email})
+    return res.json({token, nombre:user.nombre, email:user.email, id:user.id})
   } catch (error) {
     console.log(error)
     return res.status(error.code || 500).send(error)
